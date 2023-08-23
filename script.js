1c6530425542be73b8765f2fe4efa1f9094cc373
Rita Zurabian <rita.zurabyan@gmail.com>
2023-08-22 20:43:41 -0700

    more javascript changes

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-// Assignment code here
 
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+function generatePassword() {
+  var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+  var passwordLength = 12; 
+  var password = "";
 
-// Add event listener to generate button
+  for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset.charAt(randomIndex);
+  }
+
+  return password;
+}
 generateBtn.addEventListener("click", writePassword);
